dee77789be16e9b3076414a44ffc4ffb573cb567
dflong <1197083642@qq.com>
2026-03-09 16:37:17 +0800

    cursor

---
+++ src/main/java/com/dflong/algorithm/daily/ListNode.java
@@ -0,0 +1,10 @@
+package com.dflong.algorithm.daily;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode pre;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+}
---
+++ src/main/java/com/dflong/algorithm/daily/_1980.java
@@ -0,0 +1,17 @@
+package com.dflong.algorithm.daily;
+
+public class _1980 {
+
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        StringBuilder sb = new StringBuilder();
+
+        // 第idx位取nums[idx]的第idx位相反数
+        for (int i = 0; i < n; i ++) {
+            char c = nums[i].charAt(i);
+            sb.append(c == '0' ? "1" : "0");
+        }
+
+        return sb.toString();
+    }
+}
